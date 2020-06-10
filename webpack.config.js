f882cee3c65880095ef5376f915cb3e12d803888
Max Carey <mh.carey@pm.me>
2020-06-10 06:30:22 -0500

    Configure ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -11,15 +11,23 @@ module.exports = {
         libraryTarget: 'umd'
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             }
-        }]
+        ]
     }
 };
